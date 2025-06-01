ade9e46aee91f068500c66065b5976c7d3d280ce
Jyotsna <138552575+Jyo2603@users.noreply.github.com>
2025-06-01 13:12:10 +0530

    Create 1-a.c

---
+++ 1-a.c
@@ -0,0 +1,16 @@
+// 1a. Write a C program to display the file content in reverse order using lseek
+system call.
+
+#include <fcntl.h>
+#include <unistd.h>
+int main() {
+    int fd = open("f.txt", O_RDONLY);
+    off_t pos = lseek(fd, 0, 2);
+    char c;
+    while (--pos >= 0) {
+        lseek(fd, pos, 0);
+        read(fd, &c, 1);
+        write(1, &c, 1);
+    }
+    return 0;
+}
